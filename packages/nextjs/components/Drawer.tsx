3837756eac0797e3755c6812e6dc5ee0b3e2896c
Chad Bro <ayechan.sant@gmail.com>
2024-01-18 14:12:30 -0800

    basic layout done

--- packages/nextjs/components/Drawer.tsx
+++ packages/nextjs/components/Drawer.tsx
@@ -2,6 +2,7 @@ import { AddMember } from './AddMember';
 import { AddTask } from './AddTask';
 import { VotingQue } from './VotingQue';
 import { TaskDisplay } from './TaskDisplay';
+import { Reputation } from './Reputation';
 
 export const Drawer = () => {
   return (
@@ -9,16 +10,17 @@ export const Drawer = () => {
       <input id="my-drawer" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
         {/* Page content here */}
-        {/* ReputationDisplay goes here */}
-        {/* TaskDisplay goes here */}
         <div className="flex flex-row justify-between items-center">
-          {/* <div className=""> */}
           <label htmlFor="my-drawer" className="btn btn-primary drawer-button">
             Open drawer
           </label>
-          {/* </div> */}
-          <div className="flex flex-row-reverse pr-10">
-            <TaskDisplay />
+          <div>
+            <div>
+              <Reputation />
+            </div>
+            <div className="flex flex-row-reverse pr-10">
+              <TaskDisplay />
+            </div>
           </div>
         </div>
       </div>
---
+++ packages/nextjs/components/Reputation.tsx
@@ -0,0 +1,11 @@
+export const Reputation = () => {
+  return (
+    <>
+      <div className="card max-w-screen bg-base-100 shadow-xl mb-3">
+        <div className="card-body">
+          <h2 className="card-title">Reputation</h2>
+        </div>
+      </div>
+    </>
+  );
+};
