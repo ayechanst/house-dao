742eb4eadcba4eb12b517a349df3d7003589ce53
Chad Bro <ayechan.sant@gmail.com>
2024-01-19 12:55:31 -0800

    tasksDisplay.tsx done

--- packages/nextjs/components/TaskDisplay.tsx
+++ packages/nextjs/components/TaskDisplay.tsx
@@ -1,13 +1,40 @@
+import { useScaffoldContractRead } from '~~/hooks/scaffold-eth';
 import { Task } from './Task';
+
+interface TaskObject {
+  name: string;
+  manager: string;
+  taskForce: string[];
+  status: number;
+  index: number;
+}
+
 export const TaskDisplay = () => {
+  const activeTasks: TaskObject[] = [];
+  const { data: taskObjectArray } = useScaffoldContractRead({
+    contractName: 'YourContract',
+    functionName: 'getTasks',
+  });
+
+  taskObjectArray?.forEach(task => {
+    if (task.status != 0) {
+      activeTasks.push(task);
+    }
+  });
+
   return (
     <>
       <div className="grid grid-cols-4 gap-3">
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
+        {activeTasks?.map(task => {
+          <Task
+            key={task.index}
+            taskName={task.name}
+            taskManager={task.manager}
+            taskForce={task.taskForce}
+            taskStatus={task.status}
+            taskIndex={task.index}
+          />;
+        })}
       </div>
     </>
   );
